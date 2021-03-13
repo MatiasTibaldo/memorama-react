b5f5f909078968cc7a683197bbcf84c3aa3e68a2
Matias Tibaldo <mtibaldo@matriz.com.ar>
2021-03-13 01:09:55 -0300

    Eliminado de prop effect y correccion css

--- src/ui/Board.js
+++ src/ui/Board.js
@@ -5,7 +5,7 @@ import Card from './Card';
 export default class Board  extends Component{
     render(){
         return(
-            <div className='board'>{this.props.deck.map((card, index)=><Card key={index} topic={card.topic} effect={card.effect} />)}</div>
+            <div className='board'>{this.props.deck.map((card, index)=><Card key={index} topic={card.topic} />)}</div>
         )
     }
 }
--- src/ui/ImageRandom.js
+++ src/ui/ImageRandom.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ImageRandom = ({width, height, topic, effect}) => {
+const ImageRandom = ({width, height, topic}) => {
     const url = `https://source.unsplash.com/${width}x${height}/?${topic}`;
     return (
         <img src={url} alt={height}/>
